f7f46bbd1474ed2ea0096174149dcf9776a259bd
Himanshu Rawat <himanshurawat180@gmail.com>
2023-04-19 11:42:23 +0530

    Favorite feature added

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -107,7 +107,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
                         $ {price}
                     </div>
                     {!reservation && (
-                        <div className="font-light">night</div>
+                        <div className="font-light">/ night</div>
                     )}
                 </div>
                 {onAction && actionLabel && (
