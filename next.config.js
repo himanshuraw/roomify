e43f71cf2b87e1406c831ca1ae41a1f70f625032
Himanshu Rawat <himanshurawat180@gmail.com>
2023-04-23 17:12:25 +0530

    added unsplash

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: [
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com",
-      "res.cloudinary.com"
-    ]
-  }
-}
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		domains: [
+			'avatars.githubusercontent.com',
+			'lh3.googleusercontent.com',
+			'res.cloudinary.com',
+			'images.unsplash.com',
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
